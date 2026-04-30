0492434cd7b401263e270b1c100471a94ef8cc73
pangsianglian <angelpsl@ymail.com>
2026-04-30 09:51:47 +0800

    Initial SpellMaster PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'spellmaster-cache-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install the worker and cache the files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve files from the cache if offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
